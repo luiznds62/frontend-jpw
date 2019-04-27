36e29fc3769aa8b2c0c4a068cce9dd87ef09a162
luiznds62 <42879594+luiznds62@users.noreply.github.com>
2019-04-27 18:20:32 -0300

    Add files via upload

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, Injector } from '@angular/core';
-import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-login',
@@ -7,14 +6,13 @@ import { ToastrService } from 'ngx-toastr';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  _toastrService: ToastrService
 
   constructor(injector: Injector) {
-    this._toastrService = injector.get(ToastrService)
+
    }
 
   ngOnInit() {
-    this._toastrService.success('Vai toma no cu porra!')
+    
   }
 
 }
