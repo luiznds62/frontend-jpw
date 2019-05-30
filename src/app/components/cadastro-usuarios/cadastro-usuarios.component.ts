b85e5de7aba43a1b2d2e335c58c606b585cd998b
luiznds62 <42879594+luiznds62@users.noreply.github.com>
2019-05-30 19:38:37 -0300

    Luiz

--- src/app/components/cadastro-usuarios/cadastro-usuarios.component.ts
+++ src/app/components/cadastro-usuarios/cadastro-usuarios.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { NgForm, FormGroup, FormControl } from '@angular/forms';
 import { UsuarioService } from 'src/app/services/usuario.service';
 import { ToastrService } from 'ngx-toastr';
 import { Usuario } from 'src/app/model/usuario';
@@ -11,6 +11,12 @@ import { Usuario } from 'src/app/model/usuario';
 })
 export class CadastroUsuariosComponent implements OnInit {
 
+  usuarioCadastroForm = new FormGroup({
+    login: new FormControl(''),
+    senha: new FormControl(''),
+    email: new FormControl(''),
+  });
+
   constructor(private usuarioService: UsuarioService, private toastr: ToastrService) {
     
    }
@@ -18,14 +24,15 @@ export class CadastroUsuariosComponent implements OnInit {
   ngOnInit() {
   }
 
-  cadastrar(form: NgForm){
+  Cadastrar(){
     var usuarioCadastro = new Usuario;
-    usuarioCadastro = form.value;
+    usuarioCadastro = this.usuarioCadastroForm.value;
+    console.log(usuarioCadastro);
     this.usuarioService.cadastrarUsuario(usuarioCadastro).subscribe((response: any) => {
       var resposta = JSON.parse(response);
       if(resposta.sucesso){
         this.toastr.success(resposta.mensagem,'Sucesso')
-        form.reset();
+        this.usuarioCadastroForm.reset();
       }else{
         this.toastr.error(resposta.mensagem,'Erro')
       }
